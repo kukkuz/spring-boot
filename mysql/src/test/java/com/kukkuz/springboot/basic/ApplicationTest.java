462f678ed22b4082cb42df11ef59405b7bfa5d39
varun <varunz.mailbox@gmail.com>
2018-06-10 14:32:21 +0530

    test cases

--- mysql/src/test/java/com/kukkuz/springboot/basic/ApplicationTest.java
+++ mysql/src/test/java/com/kukkuz/springboot/basic/ApplicationTest.java
@@ -2,11 +2,15 @@ package com.kukkuz.springboot.basic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-integration-test.properties")
 public class ApplicationTest {
 
     @Test
